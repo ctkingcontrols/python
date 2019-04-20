4cef73598770476e84b9abe8e698e051bc18ad32
ctkingcontrols <49733978+ctkingcontrols@users.noreply.github.com>
2019-04-20 19:58:50 -0400

    Add files via upload

--- 1_File_parsing.py
+++ 1_File_parsing.py
@@ -25,18 +25,37 @@ def func_choice(x): # this function handles most print() and input() functions f
 		print('There is a problem with the extension you have chosen')
 		return
 	elif x==9:
-		return input('\nWhat is the extension of the file you wish to READ from? (1= .txt ; 2= .xlsx): ')	
+		return input('\nWhat is the extension of the file you wish to READ from? (0 = .xlsx ; 1 = .txt  ): ')	
 	elif x==10:
-		return input('\nWhat is the extension of the file you wish to WRITE to? (1= .txt ; 2= .xlsx): ')
+		return input('\nWhat is the extension of the file you wish to WRITE to? (0 = .xlsx ; 1 = .txt  ): ')
 	elif x==11:
-		search_term = input('\n You are reading from a .xls file, type in your search term: ').lower()
-		column_start = int(input('\n  type in your start column: '))
-		column_end = int(input('\n  type in your end column: '))
-		row_start = int(input('\n  type in your start row: '))
-		row_end = int(input('\n  type in your end row: '))
-		return search_term, column_start, column_end, row_start, row_end
+		user_term = input('\n You are reading from a .xls file, searching for ( 0 = Module Number ): ')
+		if user_term == '0':
+			search_term = 'Module Number'
+		else:
+			sys.exit()
+		user_range = input('\n Choose searching range (0 = default, any = custom): ')
+		if user_range == '0':
+			column_start, column_end, row_start, row_end = 0 , 20, 0 , 20
+		else:
+			column_start = input('\n  type in your start column: ')
+			column_end = input('\n  type in your end column: ')
+			row_start = input('\n  type in your start row: ')
+			row_end = input('\n  type in your end row: ')
+		return str(search_term), int(column_start), int(column_end), int(row_start), int(row_end)
+	elif x==12:
+		user_range = input('\n Choose the column range of your table (rows will be automatically calculated) (0 = 0 - 20 (default choice); any_key = custom ')
+		if user_range == '0':
+			column_start, column_end = 0 , 20
+		else:
+			column_start = input('\n  type in your start column: ')
+			column_end = input('\n  type in your end column: ')
+		return int(column_start), int(column_end)
 	else:
 		return 'Error'
+		
+		
+
 	
 
 
@@ -54,6 +73,7 @@ def func_select():			# This function gets the current directory and asks the use
 		if f_confirm == '1':
 			return f_choose
 		else:
+			sleep(5)
 			sys.exit()																			#exit program
 	elif f_select == '0':
 		sys.exit()																				#exit program	
@@ -63,33 +83,58 @@ def func_select():			# This function gets the current directory and asks the use
 			return f_choose
 	else:
 		func_choice(4)
+		sleep(5)
 		sys.exit()																				#exit program
 			
 			
 			
-def func_extension_read(file_target, para_extension = '1'):
+def func_extension_read(file_target):
 	
 	para_extension = func_choice(9)
 	file_name, file_extension = os.path.splitext(file_target)
 	
 	if (file_extension == '.txt') and (para_extension == '1'):
 		return file_target , para_extension
-	elif (file_extension == '.xlsx') and (para_extension == '2'):
+	elif (file_extension == '.xlsx') and (para_extension == '0'):
 		return file_target, para_extension
 	else:
 		print('file name was: ', file_name)
 		print('file extension was: ', file_extension)
 		print('para extension was: ', para_extension)
 		func_choice(5)
+		sleep(5)
 		sys.exit()
 		
 		
-def func_read(file_current, para_extension_read ):
-	if para_extension_read ==  '1':        # for reading a '.txt' file
-		search_term = func_choice(6)
-		with open(file_current) as f:   # open the file and automatically close it when this statement is done
+def func_draw_xls_table(actual_row_start, actual_row_end,  actual_col_start, actual_col_end,workbook, sheet): 			#workbook is still open from previous function call!
+	save1, save2, save3, save4, save5, save6 = actual_row_start, actual_row_end,  actual_col_start, actual_col_end,workbook, sheet
+	ret_col_start, ret_col_end = func_choice(12)
+	print('\n Column range: ', (range(actual_col_start, ret_col_end)))
+	print('\n Row range: ', range(actual_row_start, actual_row_end))
+	try:
+		for c in range(actual_col_start, ret_col_end):	
+			for r in range(actual_row_start, actual_row_end):
+				print('Working on row:', r,'column', c)
+				print(sheet.cell_value(r, c))
+		else:	
+			return True
+	except IndexError:
+		print('EXCEPTION (IndexError)- column :', c, 'row: ',r,'  The program has likely detected a column which has no values, try adjusting your column range')
+		func_draw_xls_table(save1, save2, save3, save4, save5, save6)
+		return True
+
+			
 	
-			if search_term == 'x.0':			#if the user chooses this option by entering 'x.0', the program will print the file and the return False
+	
+		
+		
+def func_read(file_current, para_extension_read ):
+	
+	if para_extension_read ==  '1':        											# for reading a '.txt' file
+		search_term = func_choice(6)
+		with open(file_current) as f:   											# open the file and automatically close it when this statement is done
+	
+			if search_term == 'x.0':												#if the user chooses this option by entering 'x.0', the program will print the file and the return False
 				for i in f:
 					print(i)
 				return False			
@@ -107,27 +152,64 @@ def func_read(file_current, para_extension_read ):
 					current_line = f.readline() 									#update the while loop
 				print('this is the list which holds the records of the appearances ', line_appearances)
 				return line_appearances   											#returns a list of tuples of the form: [ ('line', line_count1 ,'index', k1),('line', line_count2 ,'index', k2) ]
-	elif para_extension_read == '2':  												# for reading a '.xls' file
+	
+	elif para_extension_read == '0':  												# for reading a '.xls' file
 		search_term, column_start, column_end, row_start, row_end = func_choice(11) 
+		print(search_term, column_start, column_end, row_start, row_end)	
 		workbook = xlrd.open_workbook(file_current) 
-		sheet = workbook.sheet_by_index(0)
-		print(sheet.cell_value(2, 1)) 										######## <------------------------------- need proof of concept of retriving values from cells in the worksheet
-		#for i in range(column_start, column_end + 1):
-			#for k in range(row_start, row_end + 1):
-				#print('Working on row:', k,'column',i)
-				#if str(sheet.cell_value(k,i)) == search_term:
-					#print('Search term found on row:', k,'column',i)
-					#break
-			#print('for loop "K" exited')
-		#print('for loop "I" exited')
+		sheet = workbook.sheet_by_index(0)	
+		find_row_start, find_row_end, find_col_start, find_col_end, end_loop, ret_val = False, False, False, False, False, 0
+		for c in range(column_start, column_end ):
+			for r in range(row_start, row_end ):  
+				print('Working on row:', r,'column', c)
+				print(sheet.cell_value(r, c)) 				
+				if str(sheet.cell_value(r,c)).lower() == search_term.lower():
+					print('Search term found on row:', r,'column',c)
+					find_row_start, actual_row_start, find_col_start, actual_col_start = True, r, True, c
+				if str(sheet.cell_value(r,c)).lower() == '~' + search_term.lower():
+					print('Search term END found on row:', r,'column', c)
+					find_row_end, actual_row_end, find_col_end, actual_col_end = True, r, True, c
+				if find_row_start and find_row_end and find_col_start and find_col_end:
+					end_loop = True
+					break
+			if end_loop:
+				break
+		if end_loop:																#error handling for the case where someone has improperly configured the module list
+			err_val = 1																#error free -> err_val stays as 1
+			if actual_col_start != actual_col_end:
+				print('actual_col_start: ',actual_col_start ,' is not equal to', ' actual_col_end: ',actual_col_end)
+				err_val += 2
+			if actual_row_start == actual_row_end:
+				print('actual_row_start: ',actual_row_start ,' is equal to', ' actual_row_end: ',actual_row_end)
+				err_val += 4
+			if (actual_row_end - actual_row_start) <= 0:
+				print('actual_row_end: ',actual_row_end ,' minus', ' actual_row_start: ',actual_row_start, ' is equal to ',actual_row_end - actual_row_start )
+				err_val += 8
+			if err_val == 1 :
+				print('No errors in column or row configuration detected')
+				func_draw_xls_table(actual_row_start, actual_row_end,  actual_col_start, actual_col_end, workbook, sheet)
+			else:
+				print('err_val: ',err_val,' - program will exit soon')
+				sleep(10)
+				sys.exit()
+	
+		if err_val == 0:
+			print('There is something wrong with your excel configuration: Check for "~" character to end your selection ; Check for correct row/column search ranges')
+			print('err_val: ', err_val)
+			sleep(10)
+			sys.exit()
+			
+		return 															
+			
+
 					
 			
 def func_check_filename(para_extension = '1' ):
 	# 1 = '.txt'
-	# 2 = '.xls'
+	# 0 = '.xls'
 	if para_extension == '1':
 		compare_extension = '.txt'
-	elif para_extension == '2':
+	elif para_extension == '0':
 		compare_extension = '.xlsx'
 
 	cwd = os.getcwd()									#get current working directory
@@ -165,28 +247,33 @@ def func_output_to_file(input_data, para_extension):
 
 	return True
 	
+def main():
+	
 
 
-print('Initializing program - you will now select your target file')
-sleep(.2)
+	print('Initializing program - you will now select your target file')
+	sleep(.2)
+	f_target = func_select()
 
-f_target = func_select()
+	print ('Starting Step 2 - call func_extension_read \n')
+	sleep(.2)
+	f_current, para_extension_read = func_extension_read(f_target)    		#check to see if extension is valid, if so then return the f_target again.
 
-sleep(.2)
-print ('Starting Step 2\n')
+	print ('Starting Step 3 - call func_read\n')
+	sleep(.2)	
+	list_output = func_read(f_current,para_extension_read)
+	
+	print('Starting Step 4 - call func_output_to_file\n')
+	sleep(2)
+	func_output_to_file(list_output, para_extension_read)
 
-f_current, para_extension_read = func_extension_read(f_target, '1')    		#check to see if extension is valid, if so then return the f_target again.
-
-sleep(.2)
-print ('Starting Step 3\n')
-
-list_output = func_read(f_current,para_extension_read)
-print('Starting Step 4\n')
-sleep(2)
-
-func_output_to_file(list_output, para_extension_read)
-
+	return True
 
 
+
+
+### call of the main function
+
+main()
 
 
